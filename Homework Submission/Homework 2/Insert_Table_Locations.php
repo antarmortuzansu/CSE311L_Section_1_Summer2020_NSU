d098f0651c16a338cbe7dac01b5b58c4523bd38a
antarmortuzansu <golam.bhuiyan@northsouth.edu>
2020-07-24 04:47:45 +0600

    Homework 2 Submitted

--- Homework Submission/Homework 2/Insert_Table_Locations.php
+++ Homework Submission/Homework 2/Insert_Table_Locations.php
@@ -9,14 +9,12 @@ if ($error = $conn->connect_error) {
 }
 
 $sql = "INSERT INTO Locations(Location_id , Street_Address , Postal_Code , City , State_Province , Country_ID) 
-VALUES (
-(1400, '2014 Jabberwocky Rd', 26192, Southlake, Texas, US),
-(1500, '2011 Interiors Blvd',  99236, South San Francisco, California, US),
-(1700, '2004 Charade Rd', 98199, Seattle, Washington, US),
-(1800, '460 Bloor St. W'. , 'ON M5S 1X8', Toronto, Ontario, CA),
-(2500, 'Magdalane Centre- The Oxford Sc. Park', 'OX9 9ZB', Oxford, Oxford, UK)
-
-)";
+VALUES 
+(1400, '2014 Jabberwocky Rd', 26192, 'Southlake', 'Texas', 'US'),
+(1500, '2011 Interiors Blvd',  99236, 'South San Francisco', 'California', 'US'),
+(1700, '2004 Charade Rd', 98199, 'Seattle', 'Washington', 'US'),
+(1800, '460 Bloor St. W' , 'ON M5S 1X8', 'Toronto', 'Ontario', 'CA'),
+(2500, 'Magdalane Centre- The Oxford Sc. Park', 'OX9 9ZB', 'Oxford', 'Oxford', 'UK')";
 
 if (mysqli_query($conn, $sql)) {
     echo "Row Inserted in Table";
